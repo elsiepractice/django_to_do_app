0b02450e4760cee39b37c41f74579e8659c738e5
Elsie <positivity121@gmail.com>
2022-01-10 23:57:33 +0800

    create update_task.html and add update item

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks':tasks} 
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'tasks':tasks,'form':form} 
     return render(request,'tasks/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    return render(request, 'tasks/update_task.html')
